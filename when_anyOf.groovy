39e06baa6fc50b4ef2504ca527646a4ad59504e8
moksha2019 <81618348+moksha2019@users.noreply.github.com>
2025-02-13 19:14:55 +0530

    Create when_anyOf.groovy

---
+++ when_anyOf.groovy
@@ -0,0 +1,24 @@
+// pipeline for  defining  anyOf  condition
+//if any one of the   condition is  getting  true  it  will  execute the   condition
+pipeline {
+    agent any
+    stages {
+        stage ('buiildphase') {
+            steps {
+                echo " executing  the  build  stage: "
+            }
+        }
+        stage ('deploy stage'){
+            anyOf{
+                when {
+                    environemnt name: 'DEPLOY_TO', value: 'production'
+                    expression { BRANCH_NAME ==~ /(production|staging)/} 
+            }
+            }
+            
+            steps {
+                echo  "Deployed the application "
+            }
+        }
+    }
+}
